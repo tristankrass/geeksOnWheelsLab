f78390c4088db72293a276402dc5f7425b78b5c4
Tristan Krass <tristankrass@gmail.com>
2020-03-29 18:11:44 +0300

    added favicon && stuff

--- public/main.js
+++ public/main.js
@@ -1,100 +1,121 @@
-const augud = document.querySelectorAll( ".auk" ); // Augud, mis on väljakul
-const edetabel = document.querySelector( ".skoor" ); // Skoor
-const mutid = document.querySelectorAll( ".mutt" ); // Mutimehed.
+const augud = document.querySelectorAll(".auk"); // Augud, mis on väljakul
+const edetabel = document.querySelector(".skoor"); // Skoor
+const mutid = document.querySelectorAll(".mutt"); // Mutimehed.
 let viimaneAuk; // Viimane auk, kust mutimees välja piilus.
 let aegTäis = false; // Ütleb, kas mäng saab läbi või mitte.
 let skoor = 0; // Mängija skoor
 let äraVajutaMituKorda = false;
+
+const easyLevelBtn = document.querySelector(".js-easy-speed");
+const mediumLevelBtn = document.querySelector(".js-normal-speed");
+const hardLevelBtn = document.querySelector(".js-high-speed");
+const stopGamebtn = document.querySelector(".js-stop-game");
+
 const AEG = 20000;
-const secondsElement = document.querySelector( '.sekundit' );
-function suvalineAeg( min, max ) {
+const secondsElement = document.querySelector('.sekundit');
+let downloadTimer;
+
+let timer;
+
+function suvalineAeg(min, max) {
     // See funktsioon annab meile juhusliku aja.
-    return Math.round( Math.random() * ( max - min ) + min );
+    return Math.round(Math.random() * (max - min) + min);
 }
 
-function suvalineAuk( augud ) {
+let gameSettings = {
+    minSpeed: 1000,
+    maxSpeed: 2000,
+    time: 20,
+    currentGame: null
+}
+
+easyLevelBtn.addEventListener("click", function () {
+    gameSettings.minSpeed = 1000;
+    gameSettings.maxSpeed = 2000;
+})
+
+mediumLevelBtn.addEventListener("click", function () {
+    gameSettings.minSpeed = 300;
+    gameSettings.maxSpeed = 1400;
+})
+
+hardLevelBtn.addEventListener("click", function () {
+    gameSettings.minSpeed = 200;
+    gameSettings.maxSpeed = 500;
+
+})
+
+stopGamebtn.addEventListener("click", function () {
+    document.querySelector(".countdown").innerHTML = "Aeg Läbi";
+    clearInterval(downloadTimer);
+    window.clearTimeout(timer);    
+    aegTäis = false;
+})
+
+
+function suvalineAuk(augud) {
     // See funktsioon annab meile suvalise augu, kust mutimees välja hüppab.
-    const indeks = Math.floor( Math.random() * augud.length );
+    const indeks = Math.floor(Math.random() * augud.length);
     const auk = augud[indeks];
-    if ( auk === viimaneAuk ) {
+    if (auk === viimaneAuk) {
         // Meil võib tekkida olukord, kus meil mutionu piilub uuesti august välja, mida me ei soovi.
-        return suvalineAuk( augud );
+        return suvalineAuk(augud);
     }
     viimaneAuk = auk;
     return auk;
 }
 
 function mutiOnuPiilub() {
-    const aeg = suvalineAeg( 200, 1000 ); // Kutsume välja eelnevalt tehtud funktsiooni
-    const auk = suvalineAuk( augud ); // Kutsume välja eelnevalt tehtud funktsiooni
-    auk.classList.add( "piilub" ); // Teeme cssi abiga mutionu nähtavaks
 
-    setTimeout( function () {
-        auk.classList.remove( "piilub" );
+    const aeg = suvalineAeg(gameSettings.minSpeed, gameSettings.maxSpeed); // Kutsume välja eelnevalt tehtud funktsiooni
+    const auk = suvalineAuk(augud); // Kutsume välja eelnevalt tehtud funktsiooni
 
-        if ( !aegTäis ) {
+    auk.classList.add("piilub"); // Teeme cssi abiga mutionu nähtavaks
+
+    setTimeout(function () {
+        auk.classList.remove("piilub");
+
+        if (!aegTäis) {
             mutiOnuPiilub();
         }
-    }, aeg );
+        
+    }, aeg);
+
 }
 
-function stardiMänguga() {
-    if ( !äraVajutaMituKorda ) {
+function stardiMänguga(endgame = false) {
+    if (endgame) {
+        console.log(downloadTimer);
+    }
+
+    if (!äraVajutaMituKorda) {
         äraVajutaMituKorda = true;
         edetabel.textContent = 0;
         aegTäis = false;
         skoor = 0;
         mutiOnuPiilub();
 
-
-        // Timer
         let timeleft = 20;
-        let downloadTimer = setInterval( function () {
-            document.querySelector( ".countdown" ).innerHTML = timeleft + " sekundit jäänud.";
+        downloadTimer = setInterval(function () {
+            document.querySelector(".countdown").innerHTML = timeleft + " sekundit jäänud.";
             timeleft--;
-            if ( timeleft <= 0 ) {
-                clearInterval( downloadTimer );
-                document.querySelector( ".countdown" ).innerHTML = "Aeg Läbi"
+            if (timeleft <= 0) {
+                clearInterval(downloadTimer);
+                document.querySelector(".countdown").innerHTML = "Aeg Läbi"
             }
-        }, 1000 );
+        }, 1000);
 
-
-        setTimeout( function () {
+        window.timer = setTimeout(function () {
             aegTäis = true;
             äraVajutaMituKorda = false;
-        }, AEG );
+        }, gameSettings.time * 1000);
     }
 }
 
-function pihtas( e ) {
+function pihtas(e) {
     skoor += 1;
-
-    this.parentNode.classList.remove( "piilub" );
-
+    this.parentNode.classList.remove("piilub");
     edetabel.textContent = skoor;
 }
-// const proxyurl = "https://salty-headland-41998.herokuapp.com/";
 
-// const baseUrl = 'https://whac-a-mole-677b1.firebaseio.com/usernam';
-// ( function () {
-//     var cors_api_host = proxyurl;
-//     var cors_api_url = cors_api_host;
-//     var slice = [].slice;
-//     var origin = window.location.protocol + '//' + window.location.host;
-//     var open = XMLHttpRequest.prototype.open;
-//     XMLHttpRequest.prototype.open = function () {
-//         var args = slice.call( arguments );
-//         var targetOrigin = /^https?:\/\/([^\/]+)/i.exec( args[1] );
-//         if ( targetOrigin && targetOrigin[0].toLowerCase() !== origin &&
-//             targetOrigin[1] !== cors_api_host ) {
-//             args[1] = cors_api_url + args[1];
-//         }
-//         return open.apply( this, args );
-//     };
-// } )();
-// fetch( baseUrl )
-//     .then( response => response.text() )
-//     .then( contents => console.log( contents ) )
-//     .catch( () => console.log( "Can’t access " + baseUrl + " response. Blocked by browser?" ) )
-
-mutid.forEach( mutt => mutt.addEventListener( "click", pihtas ) );
+mutid.forEach(mutt => mutt.addEventListener("click", pihtas));
